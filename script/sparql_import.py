b6b09a8ec76c393f844f2f73215dd5318e40b828
sommergeo <26389306+sommergeo@users.noreply.github.com>
2022-07-28 16:38:13 +0200

    create wiki json workflow

--- script/sparql_import.py
+++ script/sparql_import.py
@@ -4,33 +4,7 @@ Created on Tue Jul 26 15:41:55 2022
 
 @author: Christian Sommer
 """
-import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
-
 from SPARQLWrapper import SPARQLWrapper, JSON
-
-# Specify the DBPedia endpoint
-sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-
-# Query for the description of "Capsaicin", filtered by language
-sparql.setQuery("""
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    SELECT ?comment
-    WHERE { <http://dbpedia.org/resource/Capsaicin> rdfs:comment ?comment
-    FILTER (LANG(?comment)='en')
-    }
-""")
-
-# Convert results to JSON format
-sparql.setReturnFormat(JSON)
-result = sparql.query().convert()
-
-# The return data contains "bindings" (a list of dictionaries)
-for hit in result["results"]["bindings"]:
-    # We want the "value" attribute of the "comment" field
-    print(hit["comment"]["value"])
-
-
 
 sparql = SPARQLWrapper("https://www.roceeh.uni-tuebingen.de/road/")
 sparql.setQuery("""
@@ -43,9 +17,19 @@ sparql.setQuery("""
     ?x wgs84_pos:long ?lon.
     ?x wgs84_pos:lat ?lat.
      }ORDER BY ?id
-""")
+    """)
 
 sparql.setReturnFormat(JSON)
 result = sparql.query().convert()
 
 print(result)
+
+try:
+    ret = sparql.queryAndConvert()
+
+    for r in ret["results"]["bindings"]:
+        print(r)
+except Exception as e:
+    print(e)
+    
+    
