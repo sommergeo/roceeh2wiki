e13b9c262add5cd904956ad3e13d4b4bbfd20502
sommergeo <26389306+sommergeo@users.noreply.github.com>
2022-07-29 15:52:41 +0200

    get sparql import and wiki export running

--- script/sparql_import.py
+++ script/sparql_import.py
@@ -4,32 +4,69 @@ Created on Tue Jul 26 15:41:55 2022
 
 @author: Christian Sommer
 """
-from SPARQLWrapper import SPARQLWrapper, JSON
+import sparql_dataframe
 
-sparql = SPARQLWrapper("https://www.roceeh.uni-tuebingen.de/road/")
-sparql.setQuery("""
-    PREFIX road: <https://www.roceeh.uni-tuebingen.de/road/>
-    PREFIX wgs84_pos: <https://www.w3.org/2003/01/geo/wgs84_pos#>
-    SELECT  ?title ?lon ?lat
-    WHERE{
-    ?x a road:Locality.
-    ?x road:Locality\#id ?title.
-    ?x wgs84_pos:long ?lon.
-    ?x wgs84_pos:lat ?lat.
-     }ORDER BY ?id
-    """)
+def road_query(culture):
 
-sparql.setReturnFormat(JSON)
-result = sparql.query().convert()
+    endpoint = "http://www.roceeh.uni-tuebingen.de:3030/road/query"
+    q = '''
+        PREFIX road: <https://www.roceeh.uni-tuebingen.de/road/>
+        PREFIX wgs84_pos: <https://www.w3.org/2003/01/geo/wgs84_pos#>
+        
+        SELECT  DISTINCT (?culture) ?title ?lon ?lat
+        WHERE {
+          ?x a road:ArchaeologicalLayer.
+          ?x road:ArchaeologicalLayer\#archstratigraphyIdArchstrat "'''+culture+'''".
+          ?x road:ArchaeologicalLayer\#localityId ?title.
+          ?y a road:Locality.
+          ?y road:Locality\#id ?title.
+          ?y wgs84_pos:long ?lon.
+          ?y wgs84_pos:lat ?lat.
+        } ORDER BY ?title
+    '''
+    df = sparql_dataframe.get(endpoint, q)
+    return df
 
-print(result)
+#x = road_query("Uluzzian")
 
-try:
-    ret = sparql.queryAndConvert()
 
-    for r in ret["results"]["bindings"]:
-        print(r)
-except Exception as e:
-    print(e)
-    
-    
+# =============================================================================
+# ALTERNATIVE with SPARQLWrapper 
+# from SPARQLWrapper import SPARQLWrapper, JSON
+# from pandas.io.json import json_normalize
+# 
+# 
+# sparql = SPARQLWrapper("http://www.roceeh.uni-tuebingen.de:3030/road/query") # URI must be http! https://stackoverflow.com/questions/65516325/ssl-wrong-version-number-on-python-request
+# sparql.setQuery("""
+#     PREFIX road: <https://www.roceeh.uni-tuebingen.de/road/>
+#     PREFIX wgs84_pos: <https://www.w3.org/2003/01/geo/wgs84_pos#>
+#     
+#     SELECT  DISTINCT (?culture) ?title ?lon ?lat
+#     WHERE {
+#       ?x a road:ArchaeologicalLayer.
+#       ?x road:ArchaeologicalLayer\#archstratigraphyIdArchstrat "Uluzzian".
+#       ?x road:ArchaeologicalLayer\#localityId ?title.
+#       ?y a road:Locality.
+#       ?y road:Locality\#id ?title.
+#       ?y wgs84_pos:long ?lon.
+#       ?y wgs84_pos:lat ?lat.
+#     } ORDER BY ?title
+#     """)
+# 
+# sparql.setReturnFormat(JSON)
+# result = sparql.query().convert()
+# 
+# 
+# 
+#  try:
+#      ret = sparql.queryAndConvert()
+#  
+#      for r in ret["results"]["bindings"]:
+#          print(r)
+#  except Exception as e:
+#      print(e)
+# 
+#     
+#     
+# df = json_normalize(result["results"]["bindings"])
+# =============================================================================
