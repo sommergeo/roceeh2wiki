6958e9c3324e4db176e4b24e80b882744ad9e5bf
sommergeo <26389306+sommergeo@users.noreply.github.com>
2023-01-18 16:11:08 +0100

    add rel paths and txt export

--- script/roceeh2wiki.py
+++ script/roceeh2wiki.py
@@ -8,14 +8,17 @@ import pandas as pd
 import sparql_dataframe
 import geopandas
 import json
+import os
 
+path = r'C:\data\git\mid-pleistocene-niches\roceeh2wiki'
 
+# query geodata from ROAD's SPARQL endpoint and export as table
 def road_query(culture):
     endpoint = "http://www.roceeh.uni-tuebingen.de:3030/road/query"
     q = '''
         PREFIX road: <https://www.roceeh.uni-tuebingen.de/road/>
         PREFIX wgs84_pos: <https://www.w3.org/2003/01/geo/wgs84_pos#>
-        
+
         SELECT  DISTINCT (?culture) ?title ?lon ?lat
         WHERE {
           ?x a road:ArchaeologicalLayer.
@@ -31,14 +34,11 @@ def road_query(culture):
     df["description"] = '[https://www.roceeh.uni-tuebingen.de/roadweb/tcpdf/localityInfoPDF/localityInfoPDF.php?locality=' + df.title.str.replace(' ', '%20') +' Summary Data Sheet]'
     return df
 
-
+# transform table to 
 def wiki_json(df, commons_description):
     # Head of JSON
     commons_license = "CC-BY-SA-4.0"
     commons_source = "Data retrieved from the [https://www.roceeh.uni-tuebingen.de/roadweb ROCEEH Out Of Africa Database (ROAD)]."
-    #commons_description = {"de": "Fundstellen des Uluzzien","en": "Uluzzian sites"}
-
-
 
     # Geodata JSON
     df = df.round({'lon': 2, 'lat': 2})
@@ -63,14 +63,16 @@ def wiki_json(df, commons_description):
         "data": geojson
     }
     wikistring = json.dumps(wikidict, ensure_ascii=False, indent=4)
-    print(wikistring)
+    return wikistring
 
 
-clist = pd.read_excel (r'D:\SOMMER\git\roceeh2wiki\data\wiki_cultures.xlsx')
+clist = pd.read_excel (path+'\data\wiki_cultures.xlsx')
 clist = clist.query('use=="T"') # Exlude unused rows
 
 
 for i,j in clist.iterrows():
-    print(i, j.enwiki_title)
-    x = wiki_json(road_query(j.road_culture), j.description)
-    print(x)
+    fname = os.path.join(path, 'output', j.road_culture+'.txt')     # create output path and filename
+    print(j.enwiki_title, i, fname)
+    x = wiki_json(road_query(j.road_culture), j.description)        # query from ROAD and paste to wiki-style json
+    with open(fname,'w') as f:                                      # write each culture to an individual file
+        f.write(x)
